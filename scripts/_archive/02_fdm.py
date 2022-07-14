01dea2edaf80eb5608dfcacf0ae7ab2fd0edb1d2
Rafael Pastrana <arpj@princeton.edu>
2022-07-13 23:20:54 -0500

    [Scripts] Archived folder

--- scripts/_archive/02_fdm.py
+++ scripts/_archive/02_fdm.py
@@ -1,110 +1,78 @@
-#!/usr/bin/env python3
-
 """
-Equilibrate the force network using the traditional force density method.
+Solve a constrained force density problem using gradient based optimization.
 """
 
 # filepath stuff
 import os
 
-# numpy
-import autograd.numpy as np
-
 # visualization matters
 from compas.colors import Color
-from compas.datastructures import Network
 from compas.geometry import Line
 from compas.geometry import Point
-from compas.geometry import length_vector
 from compas.geometry import add_vectors
-from compas.geometry import scale_vector
-from compas.utilities import rgb_to_hex
-
+from compas.geometry import length_vector
 from compas_view2.app import App
 
 # force equilibrium
 from force_density import JSON
-from force_density.network import CompressionNetwork
-from force_density.equilibrium import ForceDensity
+from force_density.datastructures import CompressionNetwork
+from force_density.equilibrium import fdm
 
 # ==========================================================================
 # Initial parameters
 # ==========================================================================
 
+view = True
 JSON_IN = os.path.abspath(os.path.join(JSON, "compression_network.json"))
-JSON_OUT = os.path.abspath(os.path.join(JSON, "compression_network_eq.json"))
-
-export_json = False
 
 # ==========================================================================
 # Load Network with boundary conditions from JSON
 # ==========================================================================
 
-network = CompressionNetwork.from_json(JSON_IN)
-reference_network = network.copy()
-print(f"Num nodes = {network.number_of_nodes()}, num edges = {network.number_of_edges()}")
+network_start = CompressionNetwork.from_json(JSON_IN)
 
 # ==========================================================================
-# Force Density - Extract data
+# Re-run force density to update model
 # ==========================================================================
 
-q = np.array(network.force_densities())
-
-# ==========================================================================
-# Force Density
-# ==========================================================================
-
-fd_state = ForceDensity()(q, network)
-
-# ==========================================================================
-# Update Geometry
-# ==========================================================================
-
-xyz = fd_state["xyz"].tolist()
-length = fd_state["lengths"].tolist()
-res = fd_state["residuals"].tolist()
-
-# update xyz coordinates on nodes
-network.nodes_xyz(xyz)
-
-# update q values and lengths on edges
-for idx, edge in enumerate(network.edges()):
-    network.edge_attribute(edge, name="length", value=length[idx])
-
-# update residuals on nodes
-for idx, node in enumerate(network.nodes()):
-    for name, value in zip(["rx", "ry", "rz"], res[idx]):
-        network.node_attribute(node, name=name, value=value)
-
-# ==============================================================================
-# Export new JSON file for further processing
-# ==============================================================================
-
-if export_json:
-    network.to_json(JSON_OUT)
-    print("Exported network to: {}".format(JSON_OUT))
+network = fdm(network_start)
 
 # ==========================================================================
 # Viewer
 # ==========================================================================
 
-viewer = App(width=1600, height=900)
+if view:
+    viewer = App(width=1600, height=900)
 
-# equilibrated arch
-viewer.add(network,
-           show_vertices=True,
-           pointsize=12.0,
-           show_edges=True,
-           linecolor=Color.teal(),
-           linewidth=4.0)
+    viewer.add(network,
+               show_vertices=False,
+               show_edges=True,
+               linecolor=Color.pink(),
+               linewidth=4.0)
 
-# reference arch
-viewer.add(reference_network, show_points=False)
+    # reference arch
+    viewer.add(network_start, show_points=False, linewidth=4.0)
 
-# draw supports
-for node in network.supports():
-    x, y, z = network.node_coordinates(node)
-    viewer.add(Point(x, y, z), color=Color.green(), size=20)
+    # draw lines betwen subject and target nodes
+    for node in network.nodes():
 
-# show le crème
-viewer.show()
+        pt = network_start.node_coordinates(node)
+        target_pt = network.node_coordinates(node)
+        viewer.add(Line(target_pt, pt))
+
+        # draw reaction forces
+        residual = network.node_attributes(node, names=["rx", "ry", "rz"])
+
+        if length_vector(residual) < 0.001:
+            continue
+
+        residual_line = Line(pt, add_vectors(pt, residual))
+        viewer.add(residual_line, color=Color.purple())
+
+    # draw supports
+    for node in network.supports():
+        x, y, z = network.node_coordinates(node)
+        viewer.add(Point(x, y, z), color=Color.green(), size=20)
+
+    # show le crème
+    viewer.show()
