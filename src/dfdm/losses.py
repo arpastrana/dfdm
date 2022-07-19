98a57a742af01bbc6ccaa13fff4d1b25b456c30f
Rafael Pastrana <arpj@princeton.edu>
2022-07-19 00:04:58 -0500

    [Losses] Loss signature includes `q`

--- src/dfdm/losses.py
+++ src/dfdm/losses.py
@@ -1,13 +1,13 @@
 import autograd.numpy as np
 
 
-def squared_loss(predictions, targets, weights):
+def squared_loss(predictions, targets, weights, q):
     """
     """
     return np.sum(weights * np.square(predictions - targets))
 
 
-def l2_loss(predictions, targets, weights):
+def l2_loss(predictions, targets, weights, q):
     """
     """
     return np.sqrt(squared_loss(predictions, targets, weights))
