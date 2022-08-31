cbf8f812688a86f83907b0a7502397e7412fab67
Rafael Pastrana <arpj@princeton.edu>
2022-08-31 14:10:10 -0400

    [Constraints] Implemented `LengthConstraint` and `ForceConstraint`

--- src/dfdm/constraints.py
+++ src/dfdm/constraints.py
@@ -1,69 +1,48 @@
-from autograd import grad
-
-from scipy.optimize import NonlinearConstraint
+import autograd.numpy as np
 
 
 class Constraint:
-    pass
+    def __init__(self, bound_low, bound_up):
+        self.bound_low = bound_low  # dict / array of shape (m,) or scalar
+        self.bound_up = bound_up  # dict / array of shape (m,) or scalar
+
+    def __call__(self, q, model):
+        """
+        The constraint function.
+        """
+        eqstate = model(q)
+        return self.constraint(eqstate)
+
+    def constraint(self, eqstate, **kwargs):
+        raise NotImplementederror
+
+
+class LengthConstraint(Constraint):
+    """
+    Set constraint bounds to the length of the edges of a network in equilibrium.
+    """
+    def constraint(self, eqstate, **kwargs):
+        """
+        The constraint function relative to a equilibrium state.
+        """
+        return eqstate.lengths
 
 
 class ForceConstraint(Constraint):
     """
-    Set constraint bounds to the force in the edge of a network in equilibrium.
+    Set constraint bounds to the length of the edges of a network in equilibrium.
     """
-    def __init__(self, key, force_min, force_max):
+    def constraint(self, eqstate, **kwargs):
         """
+        The constraint function relative to a equilibrium state.
         """
-        self.key = key
-        self.lb = force_min
-        self.ub = force_max
-
-    def __call__(self, model):
-        """
-        Below, a snapshot of scipy's documentation for NonlinearConstraint.
-
-        Parameters
-        ==========
-        fun: callable
-            The function defining the constraint.
-            The signature is fun(x) -> array_like, shape (m,).
-
-        lb, ub:  array_like
-            Lower and upper bounds on the constraint.
-            Each array must have the shape (m,) or be a scalar.
-            in the latter case a bound will be the same for all components
-            of the constraint.
-
-            Use np.inf with an appropriate sign to specify a one-sided constraint.
-            Set components of lb and ub equal to represent an equality constraint.
-            Note that you can mix constraints of different types: interval, one-sided,
-            or equality, by setting different components of lb and ub as necessary.
-
-        jac: {callable, ‘2-point’, ‘3-point’, ‘cs’}, optional
-            Method of computing the Jacobian matrix (an m-by-n matrix, where
-            element (i, j) is the partial derivative of f[i] with respect to x[j]).
-            The keywords {‘2-point’, ‘3-point’, ‘cs’} select a finite difference
-            scheme for the numerical estimation. A callable must have the following
-            signature: jac(x) -> {ndarray, sparse matrix}, shape (m, n). Default is ‘2-point’.
-        """
-        # fun = lambda q: model(q)
-        index = model.structure.edge_index[self.key]
-
-        def fun(q):
-            # TODO: Can we compute the grad func of model() once and then just
-            # pass that around?
-            eqstate = model(q)
-            return eqstate.forces[index]
-
-
-        return NonlinearConstraint(fun=fun,
-                                   lb=self.lb,
-                                   ub=self.ub,
-                                   jac=grad(fun))  # skip to use finite differences
+        return eqstate.forces
 
 
 
 if __name__ == "__main__":
+    from statistics import mean
+
     import autograd.numpy as np
     # compas
     from compas.colors import Color
@@ -77,9 +56,15 @@ if __name__ == "__main__":
     from compas_view2.app import App
 
     # static equilibrium
-    from dfdm.datastructures import ForceDensityNetwork
-
+    from dfdm.datastructures import FDNetwork
+    from dfdm.equilibrium import fdm
+    from dfdm.equilibrium import constrained_fdm
     from dfdm.equilibrium import EquilibriumModel
+    from dfdm.optimization import SLSQP
+    from dfdm.optimization import TrustRegionConstrained
+    from dfdm.goals import NetworkLoadPathGoal
+    from dfdm.losses import Loss
+    from dfdm.losses import PredictionError
 
     # ==========================================================================
     # Initial parameters
@@ -87,8 +72,11 @@ if __name__ == "__main__":
 
     arch_length = 5.0
     num_segments = 10
-    q_init = -1
+    q_init = -1.0
     pz = -0.1
+    length_min = 0.5
+    length_max = 1.0
+    optimizer = SLSQP
 
     # ==========================================================================
     # Create the geometry of an arch
@@ -104,7 +92,7 @@ if __name__ == "__main__":
     # Create arch
     # ==========================================================================
 
-    network = ForceDensityNetwork.from_lines(lines)
+    network = FDNetwork.from_lines(lines)
 
     # ==========================================================================
     # Define structural system
@@ -121,36 +109,147 @@ if __name__ == "__main__":
     network.nodes_loads([0.0, 0.0, pz], keys=network.nodes_free())
 
     # ==========================================================================
-    # Create constraints
+    # Run the force density method
     # ==========================================================================
 
-    constraint = ForceConstraint(key=(0, 1), force_min=-np.inf, force_max=0.0)
-
-    print(constraint)
-
-    # ==========================================================================
-    # Create constraints
-    # ==========================================================================
-
-    # get parameters
-    q = np.array(network.edges_forcedensities(), dtype=np.float64)
-    loads = np.array(network.nodes_loads(), dtype=np.float64)
-    xyz = np.array(network.nodes_coordinates(), dtype=np.float64)
-
-    # compute static equilibrium
-    model = EquilibriumModel(network)  # model can be instantiated in solver
-    eq_state = model(q, loads, xyz)
-
-    from functools import partial
-    model_partial = partial(model, loads=loads, xyz=xyz)
-    eq_state = model_partial(q)
+    print("\nForm found network")
+    network = fdm(network)
 
     # ==========================================================================
-    # Create constraints
+    # Report stats
     # ==========================================================================
 
-    constraint_scipy = constraint(model_partial)
+    q = list(network.edges_forcedensities())
+    f = list(network.edges_forces())
+    l = list(network.edges_lengths())
 
-    print(constraint_scipy)
+    print(f"Load path: {round(network.loadpath(), 3)}")
+    for name, vals in zip(("FDs", "Forces", "Lengths"), (q, f, l)):
 
-    print("Done!")
+        minv = round(min(vals), 3)
+        maxv = round(max(vals), 3)
+        meanv = round(sum(vals) / len(vals), 3)
+        print(f"{name}\t\tMin: {minv}\tMax: {maxv}\tMean: {meanv}")
+
+    # ==========================================================================
+    # Create loss funtion with total load path as the only goal
+    # ==========================================================================
+
+    goal = NetworkLoadPathGoal()
+    loss = Loss(PredictionError(goals=[goal]))
+
+    # ==========================================================================
+    # Constrained form-finding
+    # ==========================================================================
+
+    print("\nInverse form found network. No constraints")
+    ceq_network = constrained_fdm(network,
+                                  optimizer=optimizer(),
+                                  loss=loss)
+
+    # ==========================================================================
+    # Report stats
+    # ==========================================================================
+
+    q = list(ceq_network.edges_forcedensities())
+    f = list(ceq_network.edges_forces())
+    l = list(ceq_network.edges_lengths())
+
+    print(f"Load path: {round(ceq_network.loadpath(), 3)}")
+    for name, vals in zip(("FDs", "Forces", "Lengths"), (q, f, l)):
+
+        minv = round(min(vals), 3)
+        maxv = round(max(vals), 3)
+        meanv = round(sum(vals) / len(vals), 3)
+        print(f"{name}\t\tMin: {minv}\tMax: {maxv}\tMean: {meanv}")
+
+    # ==========================================================================
+    # Create constraint
+    # ==========================================================================
+
+    constraint = LengthConstraint(bound_low=length_min, bound_up=length_max)
+    constraints = [constraint]
+
+    # ==========================================================================
+    # Constrained form-finding with constraint
+    # ==========================================================================
+
+    print("\nInverse form found network. With constraints")
+    ceq_network = constrained_fdm(network,
+                                  optimizer=optimizer(),
+                                  loss=loss,
+                                  constraints=constraints,
+                                  maxiter=1000,
+                                  tol=1e-6)
+
+    # ==========================================================================
+    # Report stats
+    # ==========================================================================
+
+    q = list(ceq_network.edges_forcedensities())
+    f = list(ceq_network.edges_forces())
+    l = list(ceq_network.edges_lengths())
+
+    print(f"Load path: {round(ceq_network.loadpath(), 3)}")
+    for name, vals in zip(("FDs", "Forces", "Lengths"), (q, f, l)):
+
+        minv = round(min(vals), 3)
+        maxv = round(max(vals), 3)
+        meanv = round(sum(vals) / len(vals), 3)
+        print(f"{name}\t\tMin: {minv}\tMax: {maxv}\tMean: {meanv}")
+
+    # ==========================================================================
+    # Visualization
+    # ==========================================================================
+
+    viewer = App(width=1600, height=900, show_grid=True)
+
+    viewer.add(network, linewidth=1.0, linecolor=Color.grey().darkened())
+
+    eq_network = ceq_network
+
+    # equilibrated arch
+    viewer.add(eq_network,
+               show_vertices=True,
+               pointsize=12.0,
+               show_edges=True,
+               linecolor=Color.teal(),
+               linewidth=5.0)
+
+    # reference arch
+    viewer.add(network, show_points=False, linewidth=4.0)
+
+    for node in eq_network.nodes():
+
+        pt = eq_network.node_coordinates(node)
+
+        # draw lines betwen subject and target nodes
+        target_pt = network.node_coordinates(node)
+        viewer.add(Line(target_pt, pt))
+
+        # draw residual forces
+        residual = eq_network.node_residual(node)
+
+        if length_vector(residual) < 0.001:
+            continue
+
+        residual_line = Line(pt, add_vectors(pt, residual))
+        viewer.add(residual_line,
+                   linewidth=4.0,
+                   color=Color.pink())
+
+    # draw applied loads
+    for node in eq_network.nodes():
+        pt = eq_network.node_coordinates(node)
+        load = network.node_load(node)
+        viewer.add(Line(pt, add_vectors(pt, load)),
+                   linewidth=4.0,
+                   color=Color.green().darkened())
+
+    # draw supports
+    for node in eq_network.nodes_supports():
+        x, y, z = eq_network.node_coordinates(node)
+        viewer.add(Point(x, y, z), color=Color.green(), size=20)
+
+    # show le crème
+    viewer.show()
